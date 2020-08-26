a45317956349d62466aadc43d2813ddeb3d1b1cb
ixap2i <ayamashita124+124@gmail.com>
2020-08-26 18:01:28 +0900

    add user object to App.js

--- try-react-app/src/App.js
+++ try-react-app/src/App.js
@@ -2,10 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const user = {
+  firstName: 'Akane',
+  lastName: 'Yamashita',
+  imageTag: <img src="http://placehold.jp/99ccff/003366/150x150.png?text=Test%20image" alt="Test image" />
+};
+
+function formatName(user) {
+  return user.firstName + '' + user.lastName;
+}
+
+function renderUserImage(user) {
+  return user.imageTag;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello {formatName(user)} World</h1>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,6 +35,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <article>
+       {renderUserImage(user)} 
+      </article>
     </div>
   );
 }

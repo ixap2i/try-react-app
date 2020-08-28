9982306a9ce685765b74e16db3be994198e73ea8
ixap2i <ayamashita124@gmail.com>
2020-08-29 00:00:12 +0900

    refactor header

--- try-react-app/src/niconico/Header.js
+++ try-react-app/src/niconico/Header.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import './Header.css';
-
+import nico_mark from './nico_mark.png';
 
 function Header() {
   return (
     <header className="header">
 			<ul class="header__menu">
-				<li>山下</li>
-				<li>経歴</li>
-				<li>開発</li>
-				
+				<li>
+					<img src={nico_mark} class="a-headerIcon" alt="niconico" />
+				</li>
+				{functionMenu}
 			</ul>
 
 			<ul class="header__subMenu">
-				<li class="login">ログイン</li>
-				<li class="saiyo">採用</li>
-				<li>メニュー▼</li>
+				{functionSubMenu}
 			</ul>
     </header>
   );
 }
 
+const functionMenu = [<li>山下</li>, <li>経歴</li>, <li>開発</li>];
+const functionSubMenu = [<li class="login">ログイン</li>, <li class="saiyo">採用</li>, <li>メニュー▼</li>];
 export default Header;

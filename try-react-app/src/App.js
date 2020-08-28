55c34d348162c38fac471a194bc3865145765b61
ixap2i <ayamashita124+124@gmail.com>
2020-08-28 13:53:58 +0900

    modify base style

--- try-react-app/src/App.js
+++ try-react-app/src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
+import Header from './niconico/Header.js'
+// import UserContainer from './UserContainer.js'
+// import MovieContainer from './MovieContainer.js'
+// import MovieTag from './MovieTag.js'
+// import MainMovieContainer from './MainMovieContainer.js'
 import logo from './logo.svg';
+import spinner from './spinner.gif';
 import './App.css';
 
 const user = {
   firstName: 'Akane',
   lastName: 'Yamashita',
+  title: '【フロントエンドエンジニア】■1週間でReact.jsを勉強してポートフォリオを作ってみた',
+  subTitle: '▷1週間でどれだけできるのか〜説明文です',
   imageTag: <img src="http://placehold.jp/99ccff/003366/150x150.png?text=Test%20image" alt="Test image" />
 };
 
-function formatName(user) {
-  return user.firstName + '' + user.lastName;
+const topTable = React.createElement(
+  'table',
+  {className: 'o-table'},
+  'Hello World!'
+);
+
+function createTable() {
+  return React.createElement(
+    'table',
+    {className: 'o-table'},
+    'Hello World!!'
+  );
+}
+
+function renderTitle(user) {
+  return user.title;
+}
+
+function renderSubTitle(user) {
+  return user.subTitle;
 }
 
 function renderUserImage(user) {
   return user.imageTag;
 }
 
+function renderTopTable() {
+  return topTable;
+}
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
-        <h1>Hello {formatName(user)} World</h1>
+        <h2>{renderTitle(user)}</h2>
+        <h3>{renderSubTitle(user)}</h3>
 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -36,8 +68,14 @@ function App() {
         </a>
       </header>
       <article>
-       {renderUserImage(user)} 
+        {/* <UserContainer />
+        <MovieContainer />
+        <MovieTag /> */}
       </article>
+      <section>
+        {/* <MainMovieContainer /> */}
+      </section>
+      {createTable()}
     </div>
   );
 }

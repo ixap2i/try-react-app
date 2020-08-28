2e979f0493390b7cc8ee8891d3fd1a3544f8db13
ixap2i <ayamashita124@gmail.com>
2020-08-29 00:00:22 +0900

    add movie tag

--- try-react-app/src/niconico/MovieTag.js
+++ try-react-app/src/niconico/MovieTag.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import './MovieTag.css';
+
 function MovieTag() {
-  return(<div>
-      
+  return(<div id="tag">
+    <span class="a-movieTag -gray">タグ編集</span>
+      {movieTag.map((tagInfo) => (<MovieTags value={tagInfo} />))}
     </div>);
 }
 
+const movieTag = ['応募者', '山下 茜子', '東京福祉専門学校', 'Webエンジニア'];
+
+const MovieTags = ({value}) => (<span class="a-movieTag">{value}<span class="a-tag -icon">百</span></span>);
 export default MovieTag;

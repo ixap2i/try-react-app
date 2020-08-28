456715c9f395c5b3eb7d610aab7181ea6a81a340
ixap2i <ayamashita124@gmail.com>
2020-08-29 00:00:34 +0900

    add movie list

---
+++ try-react-app/src/niconico/MovieList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './MovieList.css';
+
+function MovieList() {
+  return(
+    <article class="m-anchorBox">
+      <div class="o-movieAnchor">
+          <div>
+              次の動画
+          </div>
+          <div>
+              連続再生
+          </div>
+      </div>
+      <div>
+          <img class="a-movieThumbnail" alt="movie thumbnail" />
+          <div class="">
+            
+          </div>
+      </div>
+    </article>);
+}
+export default MovieList;

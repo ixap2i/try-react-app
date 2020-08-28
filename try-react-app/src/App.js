8e83175014e3cd4eac97f0a570090fe1c145306f
ixap2i <ayamashita124@gmail.com>
2020-08-28 21:38:06 +0900

    refactor app component

--- try-react-app/src/App.js
+++ try-react-app/src/App.js
@@ -1,81 +1,13 @@
 import React from 'react';
+import Main from './niconico/Main.js'
 import Header from './niconico/Header.js'
-// import UserContainer from './UserContainer.js'
-// import MovieContainer from './MovieContainer.js'
-// import MovieTag from './MovieTag.js'
-// import MainMovieContainer from './MainMovieContainer.js'
-import logo from './logo.svg';
-import spinner from './spinner.gif';
 import './App.css';
-
-const user = {
-  firstName: 'Akane',
-  lastName: 'Yamashita',
-  title: '【フロントエンドエンジニア】■1週間でReact.jsを勉強してポートフォリオを作ってみた',
-  subTitle: '▷1週間でどれだけできるのか〜説明文です',
-  imageTag: <img src="http://placehold.jp/99ccff/003366/150x150.png?text=Test%20image" alt="Test image" />
-};
-
-const topTable = React.createElement(
-  'table',
-  {className: 'o-table'},
-  'Hello World!'
-);
-
-function createTable() {
-  return React.createElement(
-    'table',
-    {className: 'o-table'},
-    'Hello World!!'
-  );
-}
-
-function renderTitle(user) {
-  return user.title;
-}
-
-function renderSubTitle(user) {
-  return user.subTitle;
-}
-
-function renderUserImage(user) {
-  return user.imageTag;
-}
-
-function renderTopTable() {
-  return topTable;
-}
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <header className="App-header">
-        <h2>{renderTitle(user)}</h2>
-        <h3>{renderSubTitle(user)}</h3>
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <article>
-        {/* <UserContainer />
-        <MovieContainer />
-        <MovieTag /> */}
-      </article>
-      <section>
-        {/* <MainMovieContainer /> */}
-      </section>
-      {createTable()}
+      <Main />
     </div>
   );
 }
--- try-react-app/src/niconico/Main.js
+++ try-react-app/src/niconico/Main.js
@@ -1,67 +1,26 @@
 import React from 'react';
-import Header from './Header.js'
-import logo from './logo.svg';
-import spinner from './spinner.gif';
+
 import './Main.css';
-
-const user = {
-  firstName: 'Akane',
-  lastName: 'Yamashita',
-  imageTag: <img src="http://placehold.jp/99ccff/003366/150x150.png?text=Test%20image" alt="Test image" />
-};
-
-const topTable = React.createElement(
-  'table',
-  {className: 'o-table'},
-  'Hello World!'
-);
-
-function createTable() {
-  return React.createElement(
-    'table',
-    {className: 'o-table'},
-    'Hello World!!'
-  );
-}
-
-function formatName(user) {
-  return user.firstName + '' + user.lastName;
-}
-
-function renderUserImage(user) {
-  return user.imageTag;
-}
-
-function renderTopTable() {
-  return topTable;
-}
+import UserContainer from './UserContainer.js';
+import MovieTag from './MovieTag.js';
+import MainMovieContainer from './MainMovieContainer.js';
 
 function Main() {
   return (
     <div className="Main">
-      <Header />
-      <header className="App-header">
-        <h1>Hello {formatName(user)} World</h1>
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <article>
-       {renderUserImage(user)} 
-      </article>
-      {createTable()}
+      <UserContainer />
+      <MovieTag />
+      <MainMovieContainer />
     </div>
   );
 }
+
+// function createTable() {
+//   return React.createElement(
+//     'table',
+//     {className: 'o-table'},
+//     'Hello World!!'
+//   );
+// }
 
 export default Main;

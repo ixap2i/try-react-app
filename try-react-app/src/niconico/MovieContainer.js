08998f885c0ff256714779d282948d0dd5399e46
ixap2i <ayamashita124@gmail.com>
2020-08-29 22:46:38 +0900

    update movie container

--- try-react-app/src/niconico/MovieContainer.js
+++ try-react-app/src/niconico/MovieContainer.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './MovieContainer.css'
 import MovieList from './MovieList';
+import clock from './clock.svg';
+import directory from './directory.svg';
+import line from './line.svg';
+import twitter from './twitter.svg';
+import facebook from './facebook.svg';
 
 function MovieContainer() {
   return(
     <div class="o-movieContainer">
-      <section class="m-socialContent">
-        
-      </section>
+      <div class="m-movieInfoWrapper">
+        <section class="m-socialContent">
+          <img src={clock} class="a-toolIcon" alt="" />
+          <img src={directory} class="a-toolIcon" alt="" />
+          <img src={directory} class="a-toolIcon" alt="" />
+        </section>
+        <section class="m-socialContent">
+          <img src={line} class="a-toolIcon" alt="" />
+          <img src={twitter} class="a-toolIcon" alt="" />
+          <img src={facebook} class="a-toolIcon" alt="" />
+        </section>      
+      </div>
       <section class="m-movieInfoWrapper">
         <article class="m-commentList">
           コメント

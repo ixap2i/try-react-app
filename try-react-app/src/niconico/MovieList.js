e3e36d8d82ba4032bdfa863d344dac50ea93182b
ixap2i <ayamashita124@gmail.com>
2020-08-31 21:56:42 +0900

    change movie list size

--- try-react-app/src/niconico/MovieList.js
+++ try-react-app/src/niconico/MovieList.js
@@ -19,22 +19,22 @@ function MovieList() {
       <div className="o-movieListThumbnail">
           {/* <img className="a-movieThumbnail" alt="movie thumbnail" /> */}
           <div className="a-movieThumbnail"></div>
-          <span className="a-title">■採用結果</span>
+          <span className="a-title">■1週間でReact.jsを勉強してポートフォリオを作ってみた part2</span>
       </div>
       <div className="o-movieListThumbnail">
           {/* <img className="a-movieThumbnail" alt="movie thumbnail" /> */}
           <div className="a-movieThumbnail"></div>
-          <span className="a-title">■採用結果</span>
+          <span className="a-title">■1週間でReact.jsを勉強してポートフォリオを作ってみた part3</span>
       </div>
       <div className="o-movieListThumbnail">
           {/* <img className="a-movieThumbnail" alt="movie thumbnail" /> */}
           <div className="a-movieThumbnail"></div>
-          <span className="a-title">■採用結果</span>
+          <span className="a-title">■1週間でReact.jsを勉強してポートフォリオを作ってみた part4</span>
       </div>
       <div className="o-movieListThumbnail">
           {/* <img className="a-movieThumbnail" alt="movie thumbnail" /> */}
           <div className="a-movieThumbnail"></div>
-          <span className="a-title">■採用結果</span>
+          <span className="a-title">■1週間でReact.jsを勉強してポートフォリオを作ってみた 終</span>
       </div>
     </article>);
 }
